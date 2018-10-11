e1b863d9328130b3f024cfa200b5700c3e91a195
Paul <paulrorgan@gmail.com>
2018-10-11 19:18:37 -0400

    initial commit

---
+++ PS3/ps3.r
@@ -0,0 +1,42 @@
+###############################################################################
+# Author: Paul R. Organ
+# Purpose: ECON 675, PS3
+# Last Update: Oct 11, 2018
+###############################################################################
+# Preliminaries
+options(stringsAsFactors = F)
+
+# packages
+require(tidyverse) # data cleaning and manipulation
+require(magrittr)  # syntax
+require(ggplot2)   # plots
+require(xtable)    # tables for LaTeX
+
+setwd('C:/Users/prorgan/Box/Classes/Econ 675/Problem Sets/PS3')
+
+###############################################################################
+### Question 1: Non-linear Least Squares
+###############################################################################
+# Q1.9 setup
+
+# Q1.9a - estimates and statistics
+
+# Q1.9b - nonparametric bootstrap
+
+# Q1.9c - propensity scores
+
+###############################################################################
+### Question 2: Semiparametric GMM with Missing Data
+###############################################################################
+# Q2.2b - feasible estimator
+
+# Q2.3c - feasible estimator
+
+###############################################################################
+### Question 3: When Bootstrap Fails
+###############################################################################
+# Q3.1 - nonparametric bootstrap
+
+# Q3.2 - parametric bootstrap
+
+###############################################################################
