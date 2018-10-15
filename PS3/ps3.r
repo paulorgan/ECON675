1e597d7c881034ca7644a78209add4bb00e0be11
Paul <paulrorgan@gmail.com>
2018-10-15 19:45:40 -0400

    Q1 in R
    started with logistic regression;
    need to extract statistics and figure out bootstrap

--- PS3/ps3.r
+++ PS3/ps3.r
@@ -1,7 +1,7 @@
 ###############################################################################
 # Author: Paul R. Organ
 # Purpose: ECON 675, PS3
-# Last Update: Oct 11, 2018
+# Last Update: Oct 15, 2018
 ###############################################################################
 # Preliminaries
 options(stringsAsFactors = F)
@@ -19,7 +19,21 @@ setwd('C:/Users/prorgan/Box/Classes/Econ 675/Problem Sets/PS3')
 ###############################################################################
 # Q1.9 setup
 
+# read in data
+df <- read_csv('pisofirme.csv')
+
+# add variable to indicate having outcome data
+df %<>% mutate(s = 1-dmissing)
+
+# add log variable for us in regression [log(S_incomepc + 1)]
+df %<>% mutate(log_SincpcP1 = log(S_incomepc + 1))
+
+# logistic regression model
+reg <- glm(s ~ S_age + S_HHpeople + log_SincpcP1,
+           data = df, family = binomial(link = 'logit'))
+
 # Q1.9a - estimates and statistics
+
 
 # Q1.9b - nonparametric bootstrap
 
