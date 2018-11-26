802a4e734c6317666cde79306d1c91d9dd15520f
Paul <paulrorgan@gmail.com>
2018-11-26 15:34:06 -0500

    Q2.1
    RD Plots and Falsification Tests in Stata and R; using packages from https://sites.google.com/site/rdpackages/home

--- PS6/ps6.do
+++ PS6/ps6.do
@@ -1,7 +1,7 @@
 ********************************************************************************
 * Author: Paul R. Organ
 * Purpose: ECON 675, PS6
-* Last Update: Oct 31, 2018
+* Last Update: Nov 26, 2018
 ********************************************************************************
 clear all
 set more off
@@ -10,22 +10,55 @@ capture log close
 cd "C:\Users\prorgan\Box\Classes\Econ 675\Problem Sets\PS6"
 log using ps6.log, replace
 
+use HeadStart.dta
+
+* from the appendix
+* counties are treated if povrate60 >0, not treated o/w
+* outcome var is mort_related_post
+* preintervention var is more_related_pre
+* exog post var is mort_injury_post
+
+* packages: rdrobust, rdlocrand, rddensity, rdmulti, rdpower
+* https://sites.google.com/site/rdpackages/home
+
 ********************************************************************************
 *** Question 2: The Effect of Head Start on Child Mortality
 ********************************************************************************
 * Q2.1 (a): RD Plots
 
-* four plots: {evenly-spaced, quantile-spaced} X (IMSE-optimal,data var}
+* using the rdrobust package from Matias
+rdplot mort_related_pre povrate60, c(0) binselect(es) ///
+    graph_options(title("Evenly-spaced binning, IMSE-optimal")) saving("s\1_1a")
+graph export "s\1_1a.png", replace
+
+rdplot mort_related_pre povrate60, c(0) binselect(esmv) ///
+    graph_options(title("Evenly-spaced binning, variability-mimicking")) saving("s\1_1b")
+graph export "s\1_1b.png", replace
+
+rdplot mort_related_pre povrate60, c(0) binselect(qs) ///
+    graph_options(title("Quantile-spaced binning, IMSE-optimal"))
+graph export "s\1_1c.png", replace
+
+rdplot mort_related_pre povrate60, c(0) binselect(qsmv) ///
+    graph_options(title("Quantile-spaced binning, variability mimicking"))
+graph export "s\1_1d.png", replace
 
 ********************************************************************************
 * Q2.1 (b): Falsification Tests
 * falsification of RD design using three methods
 
 * (1) histogram plots
+twoway (hist povrate60 if povrate60 <=0, freq bcolor(black)) ///
+	   (hist povrate60 if povrate60 >0, freq bcolor(red))
+graph export "s\1_2i.png", replace
+	   
+* (2) binomial tests using rdlocrand package - how to interpret?
+rdwinselect povrate60, wmin(0.05) wstep(0.05) nwindows(100)
 
-* (2) binomial tests
-
-* (3) continuity-in-density tests
+* (3) continuity-in-density tests using rddensity package - how to interpret?
+rddensity povrate60, all
+rddensity povrate60, all h(5 5)
+rddensity povrate60, all h(1 1)
 
 ********************************************************************************
 * Q2.2 (a): constant treatment effect model
--- PS6/ps6.r
+++ PS6/ps6.r
@@ -1,7 +1,7 @@
 ###############################################################################
 # Author: Paul R. Organ
 # Purpose: ECON 675, PS6
-# Last Update: Oct 31, 2018
+# Last Update: Nov 26, 2018
 ###############################################################################
 # Preliminaries
 options(stringsAsFactors = F)
@@ -13,9 +13,15 @@ require(ggplot2)   # plots
 require(sandwich)  # robust standard errors
 require(xtable)    # tables for LaTeX
 require(stargazer) # tables for LaTeX
+require(rdrobust)  # RD stuff
+require(rdlocrand) # RD stuff
+require(rddensity) # RD stuff
 
 options(scipen = 999)
 setwd('C:/Users/prorgan/Box/Classes/Econ 675/Problem Sets/PS6')
+
+# read in dataset
+df <- read_csv('HeadStart.csv')
 
 ###############################################################################
 # Question 2) The Effect of Head Start on Child Mortality
@@ -23,16 +29,54 @@ setwd('C:/Users/prorgan/Box/Classes/Econ 675/Problem Sets/PS6')
 # Q2.1 (a): RD Plots
 
 # four plots: {evenly-spaced, quantile-spaced} X (IMSE-optimal,data var}
+png('r/1_1a.png')
+rdplot(df$mort_related_pre, df$povrate60, binselect = 'es',
+       title = 'Evenly-spaced binning, IMSE-optimal',
+       x.label = 'Poverty Rate (Normalized)',
+       y.label = 'Average Mortality Rate (Pre)')
+dev.off()
+
+png('r/1_1b.png')
+rdplot(df$mort_related_pre, df$povrate60, binselect = 'esmv',
+       title = 'Evenly-spaced binning, variability-mimicking',
+       x.label = 'Poverty Rate (Normalized)',
+       y.label = 'Average Mortality Rate (Pre)')
+dev.off()
+
+png('r/1_1c.png')
+rdplot(df$mort_related_pre, df$povrate60, binselect = 'qs',
+       title = 'Quantile-spaced binning, IMSE-optimal',
+       x.label = 'Poverty Rate (Normalized)',
+       y.label = 'Average Mortality Rate (Pre)')
+dev.off()
+
+png('r/1_1d.png')
+rdplot(df$mort_related_pre, df$povrate60, binselect = 'qsmv',
+       title = 'Quantile-spaced binning, variability mimicking',
+       x.label = 'Poverty Rate (Normalized)',
+       y.label = 'Average Mortality Rate (Pre)')
+dev.off()
 
 ###############################################################################
 # Q2.1 (b): Falsification Tests
 # falsification of RD design using three methods
 
-# (1) histogram plots
+# (i) histogram plots
+df %<>% mutate(treated = povrate60 > 0)
 
-# (2) binomial tests
+p1_2i <- ggplot(df, aes(x = povrate60, fill = treated, color = treated)) +
+  geom_histogram() + theme_minimal() +
+  labs(title = 'Histogram by Treatment Type',
+       y = 'Frequency', x = 'Poverty Rate (Normalized)')
+ggsave('r/1_2i.png')
 
-# (3) continuity-in-density tests
+# (ii) binomial tests
+rdwinselect(df$povrate60, wmin = .05, wstep = .05, nwindows = 100)
+
+# (iii) continuity-in-density tests
+rddensity(df$povrate60, all = T) %>% summary
+rddensity(df$povrate60, all = T, h = c(5,5)) %>% summary
+rddensity(df$povrate60, all = T, h = c(1,1)) %>% summary
 
 ###############################################################################
 # Q2.2 (a): constant treatment effect model
