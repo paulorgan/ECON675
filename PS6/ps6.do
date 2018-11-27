b69961351bf10530fa7d550aa37b46cb8577676f
Paul <paulrorgan@gmail.com>
2018-11-27 16:25:58 -0500

    Q2.2.1
    R and Stata;
    generate plots and table as PS requests;
    also updated var names for ease of writing throughout

--- PS6/ps6.do
+++ PS6/ps6.do
@@ -1,7 +1,7 @@
 ********************************************************************************
 * Author: Paul R. Organ
 * Purpose: ECON 675, PS6
-* Last Update: Nov 26, 2018
+* Last Update: Nov 27, 2018
 ********************************************************************************
 clear all
 set more off
@@ -21,59 +21,120 @@ use HeadStart.dta
 * packages: rdrobust, rdlocrand, rddensity, rdmulti, rdpower
 * https://sites.google.com/site/rdpackages/home
 
+* rename variable for ease of use
+rename povrate60 pov
+rename mort_related_pre rel_pre
+rename mort_related_post rel_post
+rename mort_injury_post inj_post
+
 ********************************************************************************
 *** Question 2: The Effect of Head Start on Child Mortality
 ********************************************************************************
-* Q2.1 (a): RD Plots
+* Q2.1.1: RD Plots
 
 * using the rdrobust package from Matias
-rdplot mort_related_pre povrate60, c(0) binselect(es) ///
-    graph_options(title("Evenly-spaced binning, IMSE-optimal")) saving("s\1_1a")
+rdplot rel_pre pov, c(0) binselect(es) ///
+    graph_options(title("Evenly-spaced binning, IMSE-optimal"))
 graph export "s\1_1a.png", replace
 
-rdplot mort_related_pre povrate60, c(0) binselect(esmv) ///
-    graph_options(title("Evenly-spaced binning, variability-mimicking")) saving("s\1_1b")
+rdplot rel_pre pov, c(0) binselect(esmv) ///
+    graph_options(title("Evenly-spaced binning, variability-mimicking"))
 graph export "s\1_1b.png", replace
 
-rdplot mort_related_pre povrate60, c(0) binselect(qs) ///
+rdplot rel_pre pov, c(0) binselect(qs) ///
     graph_options(title("Quantile-spaced binning, IMSE-optimal"))
 graph export "s\1_1c.png", replace
 
-rdplot mort_related_pre povrate60, c(0) binselect(qsmv) ///
+rdplot rel_pre pov, c(0) binselect(qsmv) ///
     graph_options(title("Quantile-spaced binning, variability mimicking"))
 graph export "s\1_1d.png", replace
 
 ********************************************************************************
-* Q2.1 (b): Falsification Tests
+* Q2.1.2: Falsification Tests
 * falsification of RD design using three methods
 
+gen t = pov > 0
+
 * (1) histogram plots
-twoway (hist povrate60 if povrate60 <=0, freq bcolor(black)) ///
-	   (hist povrate60 if povrate60 >0, freq bcolor(red))
+twoway (hist pov if t, freq bcolor(black)) ///
+	   (hist pov if !t, freq bcolor(red))
 graph export "s\1_2i.png", replace
 	   
 * (2) binomial tests using rdlocrand package - how to interpret?
-rdwinselect povrate60, wmin(0.05) wstep(0.05) nwindows(100)
+rdwinselect pov, wmin(0.05) wstep(0.05) nwindows(100)
 
 * (3) continuity-in-density tests using rddensity package - how to interpret?
-rddensity povrate60, all
-rddensity povrate60, all h(5 5)
-rddensity povrate60, all h(1 1)
+rddensity pov, all
+rddensity pov, all h(5 5)
+rddensity pov, all h(1 1)
 
 ********************************************************************************
-* Q2.2 (a): constant treatment effect model
+* Q2.2.1: constant treatment effect model
+
+* generate variables for polynomials of order 3-6
+gen p2 = pov^2
+gen p3 = pov^3
+gen p4 = pov^4
+gen p5 = pov^5
+gen p6 = pov^6
+
+* empty matrix to fill with point estimates and standard errors
+matrix tbl = J(2,4,.)
+
+* order 3 (run reg, grab ests, predict vals, plot, save for combine later)
+reg rel_post t pov p2 p3, vce(hc2)
+matrix tbl[1,1] = _b["t"]
+matrix tbl[2,1] = _se["t"]
+capture drop pred
+predict pred
+twoway scatter pred pov, title("Order 3")
+graph save "s\2_1a.gph", replace
+
+* order 4
+reg rel_post t pov p2 p3 p4, vce(hc2)
+matrix tbl[1,2] = _b["t"]
+matrix tbl[2,2] = _se["t"]
+capture drop pred
+predict pred
+twoway scatter pred pov, title("Order 4")
+graph save "s\2_1b.gph", replace
+
+* order 5
+reg rel_post t pov p2 p3 p4 p5, vce(hc2)
+matrix tbl[1,3] = _b["t"]
+matrix tbl[2,3] = _se["t"]
+capture drop pred
+predict pred
+twoway scatter pred pov, title("Order 5")
+graph save "s\2_1c.gph", replace
+
+* order 6
+reg rel_post t pov p2 p3 p4 p5 p6, vce(hc2)
+matrix tbl[1,4] = _b["t"]
+matrix tbl[2,4] = _se["t"]
+capture drop pred
+predict pred
+twoway scatter pred pov, title("Order 6")
+graph save "s\2_1d.gph", replace
+
+* combine graphs
+graph combine "s\2_1a.gph" "s\2_1b.gph" "s\2_1c.gph" "s\2_1d.gph"
+graph export "s\2_1.png", replace
+
+* write table for LaTeX
+mat2txt, matrix(tbl) saving("s\2_1.txt") format(%9.4f) replace
 
 ********************************************************************************
-* Q2.2 (b): heterogeneous treatment effect model
+* Q2.2.2: heterogeneous treatment effect model
 
 ********************************************************************************
-* Q2.2 (c): local parametric model
+* Q2.2.3: local parametric model
 
 ********************************************************************************
-* Q2.3 (a): MSE-optimal RD estimators
+* Q2.3.1: MSE-optimal RD estimators
 
 ********************************************************************************
-* Q2.3 (b): Robustness checks
+* Q2.3.2: Robustness checks
 
 ********************************************************************************
 * Q2.4: Local Randomization Methods
--- PS6/ps6.r
+++ PS6/ps6.r
@@ -1,7 +1,7 @@
 ###############################################################################
 # Author: Paul R. Organ
 # Purpose: ECON 675, PS6
-# Last Update: Nov 26, 2018
+# Last Update: Nov 27, 2018
 ###############################################################################
 # Preliminaries
 options(stringsAsFactors = F)
@@ -17,81 +17,143 @@ require(rdrobust)  # RD stuff
 require(rdlocrand) # RD stuff
 require(rddensity) # RD stuff
 
+source('multiplot.R') # multi-panel plots
+
 options(scipen = 999)
 setwd('C:/Users/prorgan/Box/Classes/Econ 675/Problem Sets/PS6')
 
 # read in dataset
 df <- read_csv('HeadStart.csv')
 
+# rename for ease of use
+df %<>% rename(pov = povrate60,
+               rel_post = mort_related_post,
+               rel_pre  = mort_related_pre,
+               inj_post = mort_injury_post)
+
 ###############################################################################
 # Question 2) The Effect of Head Start on Child Mortality
 ###############################################################################
-# Q2.1 (a): RD Plots
+# Q2.1.1: RD Plots
 
 # four plots: {evenly-spaced, quantile-spaced} X (IMSE-optimal,data var}
 png('r/1_1a.png')
-rdplot(df$mort_related_pre, df$povrate60, binselect = 'es',
+rdplot(df$rel_pre, df$pov, binselect = 'es',
        title = 'Evenly-spaced binning, IMSE-optimal',
        x.label = 'Poverty Rate (Normalized)',
        y.label = 'Average Mortality Rate (Pre)')
 dev.off()
 
 png('r/1_1b.png')
-rdplot(df$mort_related_pre, df$povrate60, binselect = 'esmv',
+rdplot(df$rel_pre, df$pov, binselect = 'esmv',
        title = 'Evenly-spaced binning, variability-mimicking',
        x.label = 'Poverty Rate (Normalized)',
        y.label = 'Average Mortality Rate (Pre)')
 dev.off()
 
 png('r/1_1c.png')
-rdplot(df$mort_related_pre, df$povrate60, binselect = 'qs',
+rdplot(df$rel_pre, df$pov, binselect = 'qs',
        title = 'Quantile-spaced binning, IMSE-optimal',
        x.label = 'Poverty Rate (Normalized)',
        y.label = 'Average Mortality Rate (Pre)')
 dev.off()
 
 png('r/1_1d.png')
-rdplot(df$mort_related_pre, df$povrate60, binselect = 'qsmv',
+rdplot(df$rel_pre, df$pov, binselect = 'qsmv',
        title = 'Quantile-spaced binning, variability mimicking',
        x.label = 'Poverty Rate (Normalized)',
        y.label = 'Average Mortality Rate (Pre)')
 dev.off()
 
 ###############################################################################
-# Q2.1 (b): Falsification Tests
+# Q2.1.2: Falsification Tests
 # falsification of RD design using three methods
 
 # (i) histogram plots
-df %<>% mutate(treated = povrate60 > 0)
+df %<>% mutate(t = (pov > 0) * 1)
 
-p1_2i <- ggplot(df, aes(x = povrate60, fill = treated, color = treated)) +
+p1_2i <- ggplot(df, aes(x = pov, fill = t, color = treated)) +
   geom_histogram() + theme_minimal() +
   labs(title = 'Histogram by Treatment Type',
        y = 'Frequency', x = 'Poverty Rate (Normalized)')
 ggsave('r/1_2i.png')
 
 # (ii) binomial tests
-rdwinselect(df$povrate60, wmin = .05, wstep = .05, nwindows = 100)
+rdwinselect(df$pov, wmin = .05, wstep = .05, nwindows = 100)
 
 # (iii) continuity-in-density tests
-rddensity(df$povrate60, all = T) %>% summary
-rddensity(df$povrate60, all = T, h = c(5,5)) %>% summary
-rddensity(df$povrate60, all = T, h = c(1,1)) %>% summary
+rddensity(df$pov, all = T) %>% summary
+rddensity(df$pov, all = T, h = c(5,5)) %>% summary
+rddensity(df$pov, all = T, h = c(1,1)) %>% summary
 
 ###############################################################################
-# Q2.2 (a): constant treatment effect model
+# Q2.2.1: constant treatment effect model
+
+# generate variables for polynomials of order 3-6
+df %<>% mutate(p2=pov^2, p3=pov^3, p4=pov^4, p5=pov^5, p6=pov^6)
+
+# run regressions
+r2_1a <- lm(rel_post ~ t + pov + p2 + p3, df)
+r2_1b <- lm(rel_post ~ t + pov + p2 + p3 + p4, df)
+r2_1c <- lm(rel_post ~ t + pov + p2 + p3 +p4 + p5, df)
+r2_1d <- lm(rel_post ~ t + pov + p2 + p3 + p4 + p5 + p6, df)
+
+# grab estimates for table
+tab <- matrix(NA,2,4) %>% as.data.frame()
+rownames(tab) <- c('Point Estimate', 'Standard Error')
+colnames(tab) <- c('p=3', 'p=4', 'p=5', 'p=6')
+
+# point estimates
+tab[1,1] <- r2_1a$coefficients['t']
+tab[1,2] <- r2_1b$coefficients['t']
+tab[1,3] <- r2_1c$coefficients['t']
+tab[1,4] <- r2_1d$coefficients['t']
+
+# robust standard errors
+tab[2,1] <- diag(vcovHC(r2_1a, type = "HC2")) %>% sqrt() %>% .['t']
+tab[2,2] <- diag(vcovHC(r2_1b, type = "HC2")) %>% sqrt() %>% .['t']
+tab[2,3] <- diag(vcovHC(r2_1c, type = "HC2")) %>% sqrt() %>% .['t']
+tab[2,4] <- diag(vcovHC(r2_1d, type = "HC2")) %>% sqrt() %>% .['t']
+
+# write to LaTeX
+xtable(tab)
+
+# generate dataframes for plotting
+d2_1a <- data.frame(pov = df$pov, pred = r2_1a$fitted.values)
+d2_1b <- data.frame(pov = df$pov, pred = r2_1b$fitted.values)
+d2_1c <- data.frame(pov = df$pov, pred = r2_1c$fitted.values)
+d2_1d <- data.frame(pov = df$pov, pred = r2_1d$fitted.values)
+
+# generate plot for each order
+p2_1a <- ggplot(d2_1a, aes(x = pov, y = pred)) + geom_point() +
+  theme_minimal() + labs(x = 'Pov Rate 1960 (Normalized)', y = 'Fitted Values',
+                         title = 'Order 3')
+p2_1b <- ggplot(d2_1b, aes(x = pov, y = pred)) + geom_point() +
+  theme_minimal() + labs(x = 'Pov Rate 1960 (Normalized)', y = 'Fitted Values',
+                         title = 'Order 4')
+p2_1c <- ggplot(d2_1c, aes(x = pov, y = pred)) + geom_point() +
+  theme_minimal() + labs(x = 'Pov Rate 1960 (Normalized)', y = 'Fitted Values',
+                         title = 'Order 5')
+p2_1d <- ggplot(d2_1d, aes(x = pov, y = pred)) + geom_point() +
+  theme_minimal() + labs(x = 'Pov Rate 1960 (Normalized)', y = 'Fitted Values',
+                         title = 'Order 6')
+
+# combine plots
+png('r/2_1.png')
+multiplot(p2_1a, p2_1c, p2_1b, p2_1d, cols=2)
+dev.off()
 
 ###############################################################################
-# Q2.2 (b): heterogeneous treatment effect model
+# Q2.2.2: heterogeneous treatment effect model
 
 ###############################################################################
-# Q2.2 (c): local parametric model
+# Q2.2.3: local parametric model
 
 ###############################################################################
-# Q2.3 (a): MSE-optimal RD estimators
+# Q2.3.1: MSE-optimal RD estimators
 
 ###############################################################################
-# Q2.3 (b): Robustness checks
+# Q2.3.2: Robustness checks
 
 ###############################################################################
 # Q2.4: Local Randomization Methods
